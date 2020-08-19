bb04d7d79e3b9d034827218971482cb0acee7f33
Jonathan López <jolouster@gmail.com>
2020-08-19 09:19:57 +0200

    Mejorado el alias fvim

--- myAlias.sh
+++ myAlias.sh
@@ -1,5 +1,5 @@
 ## -- JLU 12/05/2020
 alias ls='ls -lsh --color=auto'
 alias fzf='fzf --height=40% --reverse --border --prompt="-->" --preview="head -n 20 {}" --preview-window=right:50% | xargs ls -lsh'
-alias fvim='vim -o $(\fzf)'
+alias fvim='vim -o $(\fzf --height=50% --preview="head -n 20 {}" --preview-window=right:50%)'
 alias minicom='minicom -con' # habilita colores en el serial
